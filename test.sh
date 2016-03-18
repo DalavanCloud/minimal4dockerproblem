3f824b4ed3a0ab83e67563c790a6ed6712bac04a
Gerd Aschemann <gerd@aschemann.net>
2016-03-18 09:55:39 +0100

    Wait some seconds before testing ...

--- test.sh
+++ test.sh
@@ -2,6 +2,9 @@
 
 set -eu
 
+# Wait (quarter) a minute ...
+sleep 15
+
 if sudo status docker 2>/dev/null | grep -q 'process '
    then echo "Docker is up and running"
    else echo "Docker is not running!" >&2
