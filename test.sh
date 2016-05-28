ddcb644146bbe7d51afb466b6262064d6df38539
Gerd Aschemann <gerd@aschemann.net>
2016-05-28 15:28:46 +0200

    Fix: Newer "status" versions seem to return 0 even if the service is not running

--- test.sh
+++ test.sh
@@ -5,7 +5,7 @@ set -eu
 # Wait (quarter) a minute ...
 sleep 15
 
-if sudo status docker 2>/dev/null | grep -q 'process '
+if sudo docker ps 2>/dev/null | grep -q 'CONTAINER ID'
    then echo "Docker is up and running"
    else echo "Docker is not running!" >&2
         exit 1
