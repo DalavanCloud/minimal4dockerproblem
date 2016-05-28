585f2e06b8100bd937e4f0ea3ecc24f5a76b8957
Gerd Aschemann <gerd@aschemann.net>
2016-05-28 18:44:37 +0200

    Print some lines of docker log before testing

--- test.sh
+++ test.sh
@@ -5,6 +5,12 @@ set -eu
 # Wait (quarter) a minute ...
 sleep 15
 
+echo
+echo "Last 10 lines of docker log"
+echo "===================="
+sudo tail -10 /var/log/upstart/docker.log
+echo "===================="
+
 if sudo docker ps 2>/dev/null | grep -q 'CONTAINER ID'
    then echo "Docker is up and running"
    else echo "Docker is not running!" >&2
